5f544294a778b45473b762c577653432b8de3289
Leonard Chan <leonardchan1023@gmail.com>
2025-12-24 10:55:13 +0800

    Initial Commit

---
+++ LeonardCNNtest/LeonardCNNtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeonardCNNtestApp.swift
+//  LeonardCNNtest
+//
+//  Created by 陳暄暢 on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct LeonardCNNtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
